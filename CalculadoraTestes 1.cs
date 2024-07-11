ae4235d552cd2cff14235eb34ef440aad1338369
Ana Gabriela Soares <gabx.soares23@gmail.com>
2024-07-11 16:18:16 -0300

    CalculadoraTestes 1.cs

--- CalculadoraTestes 1.cs
+++ CalculadoraTestes 1.cs
@@ -10,27 +10,27 @@ namespace Teste_xUnit
         [Fact]
         public void TesteSomar()
         {
-            double resultado = Calculadora.Somar(2, 3);
+            double resultado = Calculadora.Somar(5, 3);
 
-            Assert.Equal(5, resultado);
+            Assert.Equal(8, resultado);
         }
 
 
         [Fact]
         public void TesteSubtrair()
         {
-            double resultado = Calculadora.Subtrair(5, 3);
+            double resultado = Calculadora.Subtrair(15, 5);
 
-            Assert.Equal(2, resultado);
+            Assert.Equal(10, resultado);
         }
 
 
         [Fact]
         public void TesteMultiplicar()
         {
-            double resultado = Calculadora.Multiplicar(4, 3);
+            double resultado = Calculadora.Multiplicar(5, 8);
 
-            Assert.Equal(12, resultado);
+            Assert.Equal(40, resultado);
         }
 
 
@@ -38,9 +38,9 @@ namespace Teste_xUnit
         [Fact]
         public void TesteDividir()
         {
-            double resultado = Calculadora.Dividir(27, 3);
+            double resultado = Calculadora.Dividir(30, 3);
 
-            Assert.Equal(9, resultado);
+            Assert.Equal(10, resultado);
         }
 
 
